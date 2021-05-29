b42cd69cfb6c372c310574d2820fdfda616e8267
MNShakib <mnshakib79@gmail.com>
2021-05-29 10:58:18 +0530

    about section

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,7 +15,7 @@ const Nav = () => {
       </h1>
       <ul>
         <li>
-          <Link to="/">1. About Us</Link>
+          <Link to="/">1. About Me</Link>
           <Line
             transition={{ duration: 0.75 }}
             initial={{ width: "0%" }}
@@ -23,7 +23,7 @@ const Nav = () => {
           />
         </li>
         <li>
-          <Link to="/work">2. Our Work</Link>
+          <Link to="/work">2. My Projects</Link>
           <Line
             transition={{ duration: 0.75 }}
             initial={{ width: "0%" }}
@@ -31,7 +31,7 @@ const Nav = () => {
           />
         </li>
         <li>
-          <Link to="/contact">3. Contact Us </Link>
+          <Link to="/contact">3. Contact Me </Link>
           <Line
             transition={{ duration: 0.75 }}
             initial={{ width: "0%" }}
